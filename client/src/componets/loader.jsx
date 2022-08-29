02c2a28731938ac97046a78810f0bc847250b139
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2022-08-29 09:29:05 +0300

    style loader

--- client/src/componets/loader.jsx
+++ client/src/componets/loader.jsx
@@ -3,9 +3,10 @@
 const Loader = () => {
 
   return (
-    <div className="w-full h-10 flex items-center justify-center">
-      <div className="loader h-8 w-8 self-center m-10 
-                      rounded-full border-2 border-solid 
+    <div className="w-screen h-screen flex items-center justify-center 
+                  bg-black-dark bg-opacity-90 fixed top-0 left-0">
+      <div className="loader h-10 w-10 self-center m-10 
+                      rounded-full border-4 border-solid 
                     border-grey-dark border-t-grey-light animate-spin">
       </div>
     </div>
