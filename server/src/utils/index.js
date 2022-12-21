7ec51332973b7c3130de6bb31aa4b91230b217f6
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2022-12-21 11:37:56 +0200

    add initial pagination function

--- server/src/utils/index.js
+++ server/src/utils/index.js
@@ -1,8 +1,10 @@
 const asyncHandler = require('./asyncHandler')
+const paginate = require('./paginate')
 const cloudinary = require('./cloundinary')
 
 
 module.exports = { 
   asyncHandler,
-  cloudinary 
+  paginate,
+  cloudinary
 }
---
+++ server/src/utils/paginate.js
@@ -0,0 +1,16 @@
+const paginate = async (schema, pageNumber, find) => {
+  const page = parseInt(pageNumber) || 0
+  const limit = 20
+  const count = find ? await schema.find({find}).countDocuments() : await schema.countDocuments()
+
+  const docs = await schema.find(find ? {find} : {})
+                           .sort({createdAt: -1})
+                           .limit(limit)
+                           .skip(limit * page)
+                           .exec()
+
+  return { page, pages: Math.floor(count / limit), docs }
+}
+
+
+module.exports = paginate
