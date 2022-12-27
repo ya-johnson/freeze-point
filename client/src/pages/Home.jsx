3b79bd7385d5562c627e72ea89a598523cebf48c
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2022-12-27 11:58:38 +0200

    add initial hero to Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
-import { useLocation } from 'wouter'
+import { Link, useLocation } from 'wouter'
+import { useTopicsStore } from '../store'
 import { usePagination } from '../hooks'
 import { postService } from '../services'
 import { Pagination, PostCard, Dropdown, Loader } from '../componets'
@@ -7,6 +8,7 @@ import { Pagination, PostCard, Dropdown, Loader } from '../componets'
 
 const Home = () => {
 
+  const topics = useTopicsStore(state => state.topics)
   const [posts, setPosts] = useState()
   const [loading, setLoading] = useState(true)
   const [location, setLocation] = useLocation()
@@ -28,13 +30,31 @@ const Home = () => {
 
   return (
     <main>
+      <section className="min-h-[65vh] container flex flex-col items-center py-14 space-y-14">
+        <div className="font-covered text-center">
+          <h1 className="text-8xl uppercase">Knowledege For All</h1>
+          <p className="text-3xl">Dedicated to discovery and an original edit,</p>
+          <p className="text-3xl">Library as a collective, built by the people that make us - and you.</p>
+        </div>
+        <div className="w-2/3 flex flex-wrap items-center justify-center space-x-4">
+        {topics.map(topic => {
+            return (
+              <Link href={`/p/${topic}`} >
+                <a key={`footer-${topic}`} className="p-2 brd border mb-4 hover:bg-blue-light dark:hover:text-black">{topic}</a>
+              </Link>
+            )
+          })
+        }
+        </div>
+      </section>
+
       <section className="container min-h-screen">
 
-        <div className="relative flex items-center justify-center">
+        <div className="relative flex items-center justify-center mb-20">
           <div className="py-2 px-4 brd border z-10 bg-white dark:bg-black-dark">
             <p className="font-covered text-2xl">Latest Posts</p>
           </div>
-          <div className="w-1/2 h-[1px] absolute top-1/2 border-t-2 border-dotted"></div>
+          <div className="w-1/2 h-[1px] absolute top-1/2 brd border-t-2 border-dotted"></div>
         </div>
 
         {loading ? <Loader /> : 
