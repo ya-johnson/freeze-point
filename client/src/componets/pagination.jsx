bc4e7f6a066fd165e038c1c234a891a18d92fe5f
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2022-12-28 18:24:21 +0200

    fix Pagination component - pages map func (return)

--- client/src/componets/pagination.jsx
+++ client/src/componets/pagination.jsx
@@ -2,17 +2,19 @@
 const Pagination = ({ children, page, setPage, total, pages }) => {
 
   return (
-    <div className="relative w-full">
-      {total && <p className="absolute top-1 right-2">{total + 'Posts'}</p>}
+    <div className="relative w-full pt-12 pb-32">
+      {total && <p className="absolute top-2 right-2">{'Total Posts: ' + total}</p>}
 
       {children}
       
       {pages &&
       <div className="absolute bottom-2 left-1/2 -translate-x-1/2 flex space-x-2">
         {pages.map(num => {
-          <button className={`btn neutral-btn ${num === page && 'neutral-active-btn'}`} 
-                  onClick={() => setPage(num)}>{num}
-          </button>
+          return (
+            <button className={`btn neutral-btn ${num === page && 'neutral-active-btn'}`} 
+                    onClick={() => setPage(num)}>{num}
+            </button>
+          )
         })}
       </div>
       }
