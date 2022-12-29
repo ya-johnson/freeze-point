ea4bf75ff6f804659dc304fe4e7bb005c48d1108
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2022-12-29 11:37:08 +0200

    update useEditUser hook - add edit state options

--- client/src/hooks/useEditUser.jsx
+++ client/src/hooks/useEditUser.jsx
@@ -2,23 +2,33 @@ import { useState } from 'react'
 import { useUserStore } from '../store'
 import { userService } from '../services'
 
-const useEditUser = (closeForm) => {
+const useEditUser = () => {
 
   const user = useUserStore(state => state.user)
   const setUser = useUserStore(state => state.setUser)
+  const [edit, setEdit] = useState(false)
   const [desc, setDesc] = useState()
   const [image, setImage] = useState()
+
+  const openEdit = () => setEdit(true)
+  const closeEdit = () => setEdit(false)
+  const toggleEdit = () => edit ? setEdit(false) : setEdit(true)
 
   const updateUser = async () => {
     const updateUser = await userService.updateUser(user.token, user.id, { description: desc, image })
     setUser(updateUser)
-    closeForm()
+    closeEdit()
   }
 
   return {
+    edit,
+    setEdit,
     setDesc,
     setImage,
     updateUser,
+    openEdit,
+    closeEdit,
+    toggleEdit
   }
 }
 
