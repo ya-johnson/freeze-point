547e63b5d0d49e2477e942a3528634659e867b9f
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2022-12-29 11:03:05 +0200

    update postServices - use asyncHandler (client)

--- client/src/services/postServices.js
+++ client/src/services/postServices.js
@@ -5,106 +5,65 @@ import { toast } from 'react-toastify'
 import { toastify, asyncHandler } from '../utils'
 
 
-const getPosts = asyncHandler(async args => {
+const getPosts = asyncHandler( async args => {
   const [page] = args
   const response = await axios.get(`${BASE_URL}/posts${page > 1 ? `?page=${page}` : ''}`)
   return await response.data
 })
 
-const getUserPosts = asyncHandler(async args => {
+const getUserPosts = asyncHandler( async args => {
   const [userId, page] = args
   const response = await axios.get(`${BASE_URL}/posts/${userId}${page > 1 ? `?page=${page}` : ''}`)
   return await response.data
 })
 
-const getTopicPosts = asyncHandler(async args => {
+const getTopicPosts = asyncHandler( async args => {
   const [topic, page] = args
   const response = await axios.get(`${BASE_URL}/posts/topic/${topic}${page > 1 ? `?page=${page}` : ''}`)
   return await response.data
 })
 
-const getPost = async (postId) => {
-  try {
-    const response = await axios.get(`${BASE_URL}/posts/post/${postId}`)
-    const post = await response.data
-    return post
-  }
-  catch (err) {
-    console.log(err)
-    toast.error(err.response.data.error, toastify.autoClose)
-  }
-}
+const getPost = asyncHandler( async args => {
+  const [postId] = args
+  const response = await axios.get(`${BASE_URL}/posts/post/${postId}`)
+  return await response.data
+})
 
-const createPost = async (token, data) => {
-  console.log(data)
-  try {
-    const header = setAuthHeader(token)
-    const response = await axios.post(`${BASE_URL}/posts`, data, header)
-    const post = await response.data
-    return post
-  }
-  catch (err) {
-    console.log(err)
-    toast.error(err.response.data.error, toastify.autoClose)
-  }
-}
+const createPost = asyncHandler( async args => {
+  const [token, data] = args
+  const header = setAuthHeader(token)
+  const response = await axios.post(`${BASE_URL}/posts`, data, header)
+  return await response.data
+})
 
-const updatePost = async (token, postId, data) => {
-  try {
-    const header = setAuthHeader(token)
-    const response = await axios.put(`${BASE_URL}/posts/post/${postId}`, data, header)
-    const post = await response.data
-    console.log(post)
-    return post
-  }
-  catch (err) {
-    console.log(err)
-    toast.error(err.response.data.error, toastify.autoClose)
-  }
-}
+const updatePost = asyncHandler( async args => {
+  const [token, postId, data] = args
+  const header = setAuthHeader(token)
+  const response = await axios.put(`${BASE_URL}/posts/post/${postId}`, data, header)
+  return await response.data
+})
 
-const deletePost = async (token, postId) => {
-  try {
-    const header = setAuthHeader(token)
-    console.log(header)
-    const response = await axios.delete(`${BASE_URL}/posts/post/${postId}`, { data: { postId } , headers: header.headers})
-    const confirm = await response.data
-    toast.dark('Post deleted', toastify.autoClose)
-    return confirm
-  }
-  catch (err) {
-    console.log(err)
-    toast.error(err.response.data.error, toastify.autoClose)
-  }
-}
+const deletePost = asyncHandler( async args => {
+  const [token, postId] = args
+  const header = setAuthHeader(token)
+  const response = await axios.delete(`${BASE_URL}/posts/post/${postId}`, { data: { postId } , headers: header.headers})
+  toast.dark('Post deleted', toastify.autoClose)
+  return await response.data
+})
 
-const likePost = async (token, postId, userId) => {
-  try {
-    const header = setAuthHeader(token)
-    const response = await axios.put(`${BASE_URL}/posts/${postId}/likes`, {postId, userId}, header)
-    const likes = await response.data
-    return likes
-  }
-  catch (err) {
-    console.log(err)
-    toast.error(err.response.data.error, toastify.autoClose)
-  }
-}
+const likePost = asyncHandler( async args => {
+  const [token, postId, userId] = args
+  const header = setAuthHeader(token)
+  const response = await axios.put(`${BASE_URL}/posts/${postId}/likes`, {postId, userId}, header)
+  return await response.data
+})
 
-const commentPost = async (token, postId, userId, username, body) => {
-  try {
-    const header = setAuthHeader(token)
-    const data = { postId, userId, username, body}
-    console.log(data)
-    const response = await axios.put(`${BASE_URL}/posts/${postId}/comments`, {postId, userId, username, body}, header)
-    const comments = await response.data
-    return comments
-  } 
-  catch (err) {
-    console.log(err)
-    toast.error(err.response.data.error, toastify.autoClose)
-  }
-}
+const commentPost = asyncHandler( async args => {
+  const [token, postId, userId, username, body] = args
+  const header = setAuthHeader(token)
+  const response = await axios.put(`${BASE_URL}/posts/${postId}/comments`, {postId, userId, username, body}, header)
+  return await response.data
+})
 
 
 export {
