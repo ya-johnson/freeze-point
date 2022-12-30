cc5860a23cf9422e9be42e9130afbc012fb05b35
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2022-12-30 20:46:33 +0200

    refix paginate util - count find param

--- server/src/utils/paginate.js
+++ server/src/utils/paginate.js
@@ -1,7 +1,7 @@
 const paginate = async (schema, pageNumber, find) => {
   const page = parseInt(pageNumber) || 0
   const limit = 20
-  const count = find ? await schema.find({find}).countDocuments() : await schema.countDocuments()
+  const count = find ? await schema.find(find).countDocuments() : await schema.countDocuments()
 
   const docs = await schema.find(find ? find : {})
                            .sort({createdAt: -1})
