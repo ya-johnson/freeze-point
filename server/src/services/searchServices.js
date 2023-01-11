d912c19b4c110eb3f0d50fd9087e1592509505fc
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2023-01-11 18:19:50 +0200

    fix searchService - add regx

--- server/src/services/searchServices.js
+++ server/src/services/searchServices.js
@@ -2,8 +2,9 @@ const { User, Post } = require('../models')
 
 
 const getSearchResults = async (term) => {
-  const posts = await Post.find({title: term})
-  const users = await User.find({ name: term })
+  const value = new RegExp(term, 'i')
+  const posts = await Post.find({title: value})
+  const users = await User.find({name: value})
   return { posts, users }
 }
 
