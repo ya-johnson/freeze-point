f45ffdd28ee9d9a1297da5e4d5600dd239db5f7c
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2023-01-31 18:55:39 +0200

    update Home page - add responsive classes to hero section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -16,15 +16,15 @@ const Home = () => {
     <main>
       <section className="min-h-[65vh] container flex flex-col items-center py-14 space-y-14">
         <div className="font-covered text-center">
-          <h1 className="text-8xl uppercase">Knowledege For All</h1>
-          <p className="text-3xl">Dedicated to discovery and an original edit,</p>
-          <p className="text-3xl">Library as a collective, built by the people that make us - and you.</p>
+          <h1 className="md:text-8xl uppercase">Knowledege For All</h1>
+          <p className="md:text-3xl">Dedicated to discovery and an original edit,</p>
+          <p className="md:text-3xl">Library as a collective, built by the people that make us - and you.</p>
         </div>
-        <div className="w-2/3 flex flex-wrap items-center justify-center space-x-4">
+        <div className="md:w-2/3 flex flex-wrap items-center justify-center gap-4">
         {topics.map(topic => {
             return (
               <Link href={`/p/${topic}`} >
-                <a key={`home-${topic}`} className="btn neutral-btn mb-4">{topic}</a>
+                <a key={`home-${topic}`} className="btn neutral-btn">{topic}</a>
               </Link>
             )
           })
