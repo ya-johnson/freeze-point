0d10cc31aee0a901c6363a9362a6149388d6e662
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2023-01-31 18:23:49 +0200

    update Footer component - fix responsive layout

--- client/src/componets/footer.jsx
+++ client/src/componets/footer.jsx
@@ -1,51 +1,48 @@
 import { Link } from 'wouter'
-import { GiFrozenArrow } from 'react-icons/gi'
 import { useTopicsStore } from '../store'
 import { algs } from '../utils'
+
 
 const Footer = () => {
 
   const topics = useTopicsStore(state => state.topics)
   const topicsChunks = algs.spliceToChunks(topics, 4)
-
-  const backToTop = () => {
-    window.scrollTo({top: 0})
-  }
-
+  const backToTop = () => window.scrollTo({top: 0})
 
   return (
-    <footer className="py-20 bg-grey dark:bg-black-dark">
-      <div className="container flex items-center justify-between">
-        <div className="flex space-x-12">
+    <footer className="py-20 mt-28 bg-white dark:bg-black-dark brd border-t">
+      <div className="container flex justify-between space-x-4">
+        <div className="flex flex-col max-w-[200px] md:max-w-[500px] space-y-4">
           <Link href='/'>
-            <a><GiFrozenArrow className="h-10 w-10 hover:text-pink"/></a>
+            <a className="font-covered text-3xl hover:text-pink-light">freeze point.</a>
           </Link>
 
-          <div className="flex space-x-8">
-            { topicsChunks.map(topics => {
-              return (
-              <div className="flex flex-col space-y-1"> 
-                {topics.map(topic => {
-                  return (
-                    <Link href={`/topics/${topic}`} >
-                      <a className="footer-topic-link">{topic}</a>
-                    </Link>
-                  )
-                })}
-              </div>)
-            })
-            }
+          <div className="flex flex-wrap gap-4">
+          {topicsChunks.map(topics => {
+            return (
+              <div className="flex flex-col space-y-1">
+              {topics.map(topic => {
+                return (
+                  <Link href={`/p/${topic}`} >
+                    <a key={`footer-${topic}`} className="footer-topic-link">{topic}</a>
+                  </Link>
+                )
+              })}
+              </div>
+            )
+          })}
+
           </div>
         </div>
 
-        <div className="cursor-pointer p-4 w-28 h-28 flex flex-col  justify-center text-lg
-                      font-medium bg-black text-grey-light dark:bg-white dark:text-black"
+        <div className="cursor-pointer p-4 w-28 h-28 flex flex-col justify-center text-3xl leading-7
+                        font-covered bg-black text-white dark:bg-white dark:text-black"
              onClick={backToTop}>
           <span>Back</span>
           <span>To</span>
           <span>Top</span>
         </div>
-        </div>
+      </div>
     </footer>
   )
 }
