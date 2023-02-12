19b0370ef0d11034997ac419bd1cd8334f859b33
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2023-02-12 10:42:40 +0200

    update Latest component - mobile text size

--- client/src/componets/latest.jsx
+++ client/src/componets/latest.jsx
@@ -5,7 +5,7 @@ const Latest = () => {
   return (
     <div className="relative flex items-center justify-center mb-20">
       <div className="py-2 px-4 brd border z-10 bg-white dark:bg-black-dark">
-        <p className="font-covered text-4xl">Latest Posts</p>
+        <p className="font-covered text-2xl md:text-4xl">Latest Posts</p>
       </div>
       <div className="w-full h-[1px] absolute top-1/2 brd border-t-2 border-dotted"></div>
     </div>
