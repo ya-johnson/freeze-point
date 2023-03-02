4b86ac7e7d0e36b027311105c965926a56d6fa9a
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2023-03-02 16:25:15 +0200

    update Vite config - remove server and env vars configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
-
-  const env = loadEnv(mode, process.cwd(), '')
-
-  return {
-    plugins: [react()],
-    define: {
-      __APP_ENV__: env.APP_ENV,
-    },
-    server: {
-      host: true,
-      port: 5173
-    }
-  }
+export default defineConfig({
+  plugins: [react()],
 })
